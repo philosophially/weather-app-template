1dd92ed039fb9d75546365bc29690787d452c192
sophia <sophialeeloves@gmail.com>
2022-05-08 19:59:22 +0800

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -46,10 +46,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "c3333c25f62df25da6b4c9597ac6f645";
-let city = "Boston";
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "c3333c25f62df25da6b4c9597ac6f645";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Boston");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
